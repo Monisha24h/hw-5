641a6a6882493407b233ab033cd5846f458727bc
Monisha24h <monima824@gmail.com>
2021-06-28 10:12:46 +0600

    fitst

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+    $('.fadeOut').owlCarousel({
+      items:1,
+          margin:15,
+          autoplay:true,
+          autoplayTimeout:3000,
+          loop:true,
+    });
+  });
+  
